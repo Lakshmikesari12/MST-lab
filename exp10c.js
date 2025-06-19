2265b8989bfc4a336480fd90313a756189c437cc
Lakshmikesari12 <lakshmireddykesari123@gmail.com>
2025-06-19 13:16:51 +0530

    Delete exp10c.js

--- exp10c.js
+++
@@ -1,5 +0,0 @@
-function displayProductDetails(product) {
-    console.log("Product ID: ".concat(product.productId, ", Product Name: ").concat(product.productName));
-}
-var newProduct = { productId: 1234, productName: "Widget" };
-displayProductDetails(newProduct);
