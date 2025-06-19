1850dfc93692d21d2e2803c82b1af95f0f2ad058
Lakshmikesari12 <lakshmireddykesari123@gmail.com>
2025-06-19 13:16:08 +0530

    Delete exp10b.js

--- exp10b.js
+++
@@ -1,6 +0,0 @@
-function createProduct(id, name) {
-    var product = { productId: id, productName: name, };
-    return product;
-} // Example usage
-var newProduct = createProduct(1234, "Widget");
-console.log(newProduct); // { productId: 1234, productName: "Widget" }
