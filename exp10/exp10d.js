74d851895108a2b458696326b65f659371eb2d41
Lakshmikesari12 <lakshmireddykesari123@gmail.com>
2025-06-19 13:18:56 +0530

    Add files via upload

---
+++ exp10/exp10d.js
@@ -0,0 +1,6 @@
+// Define a function that matches the function type defined in the interface
+var add = function (a, b) { return a + b; };
+// Access the value of the function type
+var result = add(5, 9);
+console.log(result);
+// Output: 14
